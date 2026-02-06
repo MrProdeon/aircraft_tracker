5e34b7b03f19ca1f1476fdeaa6e782a7faae9961
Prodeon <prodeon21@gmail.com>
2026-02-06 21:09:57 +0300

    Создан класс для подключения к апи openstreetmap и opensky, добавлены методы для получения координат и информации о самолетах. Начало работы над классом Plane

---
+++ src/APIAdapter.py
@@ -0,0 +1,75 @@
+import json
+from abc import ABC, abstractmethod
+import requests
+
+class AbstractAdapter(ABC):
+
+    @abstractmethod
+    def get_coordinates(self, country):
+        pass
+
+    @abstractmethod
+    def get_aeroplanes(self, country : str) -> None:
+        pass
+
+
+class APIAdapter(AbstractAdapter):
+
+    def __init__(self):
+        self.openstreetmap_url = "https://nominatim.openstreetmap.org/search"
+        self.opensky_url = "https://opensky-network.org/api/states/all?"
+        self.aeroplanes = None
+
+
+    def get_coordinates(self, country):
+        headers_nominatim = {
+            'User-Agent': 'test-app/1.0',
+        }
+
+        params_nominatim = {
+            'country': country,
+            'format': 'json',
+            'limit': 1,
+        }
+
+        response = requests.get(url=self.openstreetmap_url, params=params_nominatim, headers=headers_nominatim)
+
+        data = response.json()
+
+        geo_coordinates = data[0].get("boundingbox")
+
+        return geo_coordinates
+
+
+
+    def get_aeroplanes(self, geo_coordinates : list) -> None:
+
+        params = {
+            'lamin': geo_coordinates[0],
+            'lamax': geo_coordinates[1],
+            'lomin': geo_coordinates[2],
+            'lomax': geo_coordinates[3],
+        }
+
+        response = requests.get(url=self.opensky_url, params=params)
+
+        self.aeroplanes = response.json()
+
+
+
+# test = APIAdapter()
+# coordinates = test.get_coordinates("Russia")
+# test.get_aeroplanes(coordinates)
+# print(test.aeroplanes)
+
+
+class Plane:
+
+    def __init__(self, country, callsign, speed, geo_altitude):
+        self.country = country
+        self.callsign = callsign
+        self.speed = speed
+        self.geo_altitude = geo_altitude
+
+
+    pass
